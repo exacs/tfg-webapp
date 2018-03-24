69d83d4200b70691d058bee91536d8051df5bcd1
Carlos Saito <c.saito.style@gmail.com>
2018-03-24 12:07:33 +0100

    Add koa-router and routes "/" & "/query"

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 const Koa = require('koa');
 const app = new Koa();
+const _ = require('koa-route');
+
+app.use(_.get('/', async ctx => {
+  console.log('ENDPOINT /');
+}));
+
+app.use(_.get('/query', async ctx => {
+  console.log('ENDPOINT /query');
+}));
 
 app.use(async ctx => {
   ctx.body = 'Hello World';
