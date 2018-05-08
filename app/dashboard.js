90ff4ac45b29de234f4ffff07f40c4108659114b
Carlos Saito <c.saito.style@gmail.com>
2018-05-08 20:54:01 +0200

    Fix dashboard.js

--- app/dashboard.js
+++ app/dashboard.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import App from './scenes/dashboard';
+import App from './components/app';
 
 ReactDOM.render(<App />, document.getElementById('root'));
