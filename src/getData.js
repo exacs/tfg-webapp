b31fd9ed76b52758fa3660a0eedbb34a562f9a4c
Carlos Saito <c.saito.style@gmail.com>
2018-05-19 10:15:55 +0200

    Update API: now returns origin-destination-amount vectors

--- src/getData.js
+++ src/getData.js
@@ -1,12 +1,13 @@
 const data = {
   2015: [
-    ['es',   5000],
-    ['fr',  15000],
-    ['ma', -20000]
-  ],
-  2016: [
-    ['es', 20000],
-    ['fr',  1500]
+    {origin: 'sy', destination: 'fr', amount: 10000},
+    {origin: 'sy', destination: 'es', amount:  3000},
+    {origin: 'ma', destination: 'es', amount: 12000},
+    {origin: 'ma', destination: 'fr', amount:  5000},
+    {origin: 'es', destination: 'fr', amount:  1000},
+    {origin: 'sy', destination: 'se', amount: 15000},
+    {origin: 'es', destination: 'se', amount:  5500},
+    {origin: 'fr', destination: 'se', amount:  2000}
   ]
 }
 
@@ -14,5 +15,5 @@ module.exports = async function getData (year, dimension) {
   // Read the data from the database
   // Link those data to Wikidata
   // Return
-  return data[year] || data['2016'];
+  return data[year] || data['2015'];
 }
