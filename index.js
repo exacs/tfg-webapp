4fda9fb0d04d12dcde989ef3f954bf605b8ba3ff
Carlos Saito <c.saito.style@gmail.com>
2018-05-30 10:34:30 +0200

    Fix deploy

--- index.js
+++ index.js
@@ -47,5 +47,8 @@ if (process.env.NODE_ENV === 'development') {
   }));
 }
 
+const PORT = process.env.PORT || 3000;
 
-app.listen(3000);
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}`);
+});
